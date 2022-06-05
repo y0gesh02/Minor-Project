8cef811b46127138f525f5ddc85d7edd901c0c3c
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-06 00:24:50 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://db_yogesh:devil12345@cluster0.wjfvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.DATABASE;
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
